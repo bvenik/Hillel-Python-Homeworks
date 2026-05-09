28d48c668ea3020516009619d03e5df4417b38fe
bebrik-varenik <xd0dimka@gmail.com>
2026-05-09 20:41:49 +0300

    Added docstrings & type hints

--- hw15/my_site/home/apps.py
+++ hw15/my_site/home/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    """app config for my_site project. made by Django"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
--- hw15/my_site/home/views.py
+++ hw15/my_site/home/views.py
@@ -1,25 +1,66 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 
 
-def home_view(request):
+def home_view(request: HttpRequest) -> HttpResponse:
+    """
+    Displays a welcome message on the home page
+
+    :param request: HTTP request
+    :return: response with a welcome string
+    """
     return HttpResponse("Ласкаво просимо на головну сторінку")
 
 
-def about_view(request):
+def about_view(request: HttpRequest) -> HttpResponse:
+    """
+    Displays the about page message
+
+    :param request: HTTP request
+    :return: response with about string
+    """
     return HttpResponse("Сторінка про нас")
 
 
-def contact_view(request):
+def contact_view(request: HttpRequest) -> HttpResponse:
+    """
+    Display contact information message
+
+    :param request: HTTP request
+    :return: response with contact details string
+    """
     return HttpResponse("Зв'яжіться з нами")
 
 
-def post_view(request, id):
+def post_view(request: HttpRequest, id: int) -> HttpResponse:
+    """
+    Display a specific post identified by ID
+
+    :param request: HTTP request
+    :param id: ID of post
+    :return: response showing the post ID
+    """
     return HttpResponse(f"Ви переглядаєте пост з ID: {id}")
 
 
-def profile_view(request, username):
+def profile_view(request: HttpRequest, username: str) -> HttpResponse:
+    """
+    Display the profile of a specific user
+
+    :param request: HTTP request
+    :param username: username
+    :return: response showing the username
+    """
     return HttpResponse(f"Ви переглядаєте профіль користувача: {username}")
 
 
-def event_view(request, year, month, day):
+def event_view(request: HttpRequest, year: int, month: int, day: int) -> HttpResponse:
+    """
+    Display the date of a specific event
+
+    :param request: HTTP request
+    :param year: year of the event
+    :param month: month of the event
+    :param day: day of the event
+    :return: response showing the date
+    """
     return HttpResponse(f"Дата події: {year}-{month}-{day}")
