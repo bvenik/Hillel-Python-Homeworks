66b349b1ba1766af64ad565547dd004847991604
bebrik-varenik <xd0dimka@gmail.com>
2026-07-16 00:26:06 +0300

    added main page to urls

--- hw26/myapp/urls.py
+++ hw26/myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home_view, name='home'),
+    path('', views.main_page_view, name='main_page'),
 ]
--- hw26/myapp/views.py
+++ hw26/myapp/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def home_view(request):
     return HttpResponse("Hello, world!")
+
+def main_page_view(request):
+    return HttpResponse('Welcome to the main page!')
